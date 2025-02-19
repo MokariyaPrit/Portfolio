2ae21f38066101cbca7a9850318920b2f935f1f9
mokariya prit <mokariyaprit1123@gmail.com>
2025-02-19 15:21:49 +0530

    deploy  commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Homesection from "./pages/Home";
 import NotFound from "./NotFound";
 
 const App = () => {
   return (
-    <>      
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Homesection />} />
-        <Route path="*" element={<NotFound />} />
-
-      </Route>
-    </Routes>
-    </>
+    <HashRouter>      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homesection />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 };
 
 export default App;
-
