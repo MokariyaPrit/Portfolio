beda88906c1e84d258ff4dea46791b50cd9cef6d
mokariya prit <mokariyaprit1123@gmail.com>
2025-02-19 15:49:18 +0530

    Fix duplicate router issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Homesection from "./pages/Home";
 import NotFound from "./NotFound";
 
 const App = () => {
   return (
-    <HashRouter>      
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homesection />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homesection />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
 
